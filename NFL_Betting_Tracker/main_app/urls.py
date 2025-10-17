b500ace507d59ad4045df5ce01412d70b27bcfc7
Rtkmak14 <Rtkmak14@gmail.com>
2025-10-17 12:04:18 -0500

    template scaffolds, views, urls

--- NFL_Betting_Tracker/main_app/urls.py
+++ NFL_Betting_Tracker/main_app/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
+from .views import NoteListView, NoteDetailView, NoteCreateView
+
 
 urlpatterns = [
-    # Routes will be added here
+    path('',views.home,name='home'),
+    path('notes/', NoteListView.as_view(), name='note_list'),
+    path('notes/<int:pk>/', NoteDetailView.as_view(), name='note_detail'),
+    path('notes/new/', NoteCreateView.as_view(), name='note_create'),
 ]
--- NFL_Betting_Tracker/main_app/views.py
+++ NFL_Betting_Tracker/main_app/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
+from .models import Note
 
 # Create your views here.
+
+def home(request):
+    return render(request,'home.html')
+
+class NoteListView(ListView):
+    model = Note
+    template_name = 'notes/note_list.html'
+    context_object_name = 'notes'
+
+class NoteDetailView(DetailView):
+    model = Note
+    template_name = 'notes/note_detail.html'
+
+class NoteCreateView(CreateView):
+    model = Note
+    fields = ['team', 'game_date', 'spread', 'total', 'moneyline', 'prior_week_winning_margin', 'travel_miles']
+    template_name = 'notes/note_form.html'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
