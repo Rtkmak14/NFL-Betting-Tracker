54ff95f1fcc019e83e12d8504ab1f4ce3d179487
Rtkmak14 <Rtkmak14@gmail.com>
2025-10-17 11:29:58 -0500

    Init commit (app creation, setting config, model creation)

---
+++ NFL_Betting_Tracker/main_app/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+class Note(models.Model):
+    team=models.CharField(max_length=100)
+    game_date=models.DateField()
+    spread=models.CharField(max_length=100)
+    total=models.IntegerField()
+    moneyline=models.CharField(max_length=100)
+    prior_week_winning_margin=models.IntegerField()
+    travel_miles=models.IntegerField()
+
+    def __str__(self):
+        return f"{self.team}-{self.game_date}"
+    
+
