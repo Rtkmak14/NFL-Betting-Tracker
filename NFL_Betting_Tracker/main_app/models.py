b416af6176f1dd2784064085c8a701c8f8b5f70a
Rtkmak14 <Rtkmak14@gmail.com>
2025-10-21 14:13:18 -0500

    model updates

--- NFL_Betting_Tracker/main_app/models.py
+++ NFL_Betting_Tracker/main_app/models.py
@@ -3,15 +3,14 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 
 class Note(models.Model):
-    team=models.CharField(max_length=100)
-    game_date=models.DateField()
-    spread=models.CharField(max_length=100)
-    total=models.IntegerField()
-    moneyline=models.CharField(max_length=100)
-    prior_week_winning_margin=models.IntegerField()
-    travel_miles=models.IntegerField()
-
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    team = models.CharField(max_length=100)
+    game_date = models.DateField()
+    spread = models.CharField(max_length=100)
+    total = models.CharField(max_length=100)  
+    moneyline = models.CharField(max_length=100)
+    prior_week_winning_margin = models.CharField(max_length=100)  
+    travel_miles = models.CharField(max_length=100) 
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.team}-{self.game_date}"
